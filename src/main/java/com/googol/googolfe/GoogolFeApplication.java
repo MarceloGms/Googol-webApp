b789fd1f54bca94d74782a612487beba742e1c1e
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-05-06 15:28:40 +0100

    first commit

---
+++ src/main/java/com/googol/googolfe/GoogolFeApplication.java
@@ -0,0 +1,13 @@
+package com.googol.googolfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogolFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogolFeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/googol/googolfe/GoogolFeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googol.googolfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogolFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
